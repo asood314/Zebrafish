6f17ce518a3870bb5d131a7f921be4a8a712b685
Alexander Sood <asood@nat-tvwna-inside-visitornet101-c-10018.princeton.org>
2015-07-01 12:33:10 -0400

    Skeleton of matlab file to read image data for analysis

--- ZebrafishAnalyzer.m
+++ ZebrafishAnalyzer.m
@@ -1,10 +1,12 @@
 classdef ZebrafishAnalyzer
     
     properties
+        directoryList;
         currentDirectory;
-        metaDataContainer;
+        fileMetaDataIndex;
+        posDataContainer;
         imageSeries;
-        imageMetaData;
+        imagePosData;
         currentFrame;
         %colorMap;
     end
@@ -12,49 +14,77 @@ classdef ZebrafishAnalyzer
     methods
         
         function obj = ZebrafishAnalyzer(dir, filename)
-            currentDirectory = dir;
+            obj.directoryList(1,:) = char(dir);
+            obj.currentDirectory = dir;
             fid = fopen(strcat(dir,'positionData.txt'),'r');
-            ifile = 0;
+            ifile = 1;
             while feof(fid)==0
-                metaDataContainer(ifile,1) = fgets(fid);
+                tmp = fgets(fid);
+                obj.fileMetaDataIndex(ifile,:) = tmp(1:end-1);
                 tmp = fscanf(fid,'p=(%f,%f,%f)\n');
-                metaDataContainer(ifile,2) = tmp(1);
-                metaDataContainer(ifile,3) = tmp(2);
-                metaDataContainer(ifile,4) = tmp(3);
+                obj.posDataContainer(ifile,1) = tmp(1);
+                obj.posDataContainer(ifile,2) = tmp(2);
+                obj.posDataContainer(ifile,3) = tmp(3);
                 ifile = ifile + 1;
             end
-            imageSeries = Tiff(strcat(dir,filename),'r');
-            for i=1:size(metaDataContainer(:,1)
-                metaDataFound = 0;
-                if strcmp(filename,metaDataContainer(i,1))==1
-                    imageMetaData(1) = metaDataContainer(2);
-                    imageMetaData(2) = metaDataContainer(3);
-                    imageMetaData(3) = metaDataContainer(4);
-                    metaDataFound = 1;
+            obj.imageSeries = Tiff(strcat(dir,filename),'r');
+            for i=1:length(obj.fileMetaDataIndex(:,1))
+                posDataFound = 0;
+                if strcmp(filename,char(obj.fileMetaDataIndex(i,:)))==1
+                    char(obj.fileMetaDataIndex(i,:))
+                    obj.imagePosData(1) = obj.posDataContainer(i,1);
+                    obj.imagePosData(2) = obj.posDataContainer(i,2);
+                    obj.imagePosData(3) = obj.posDataContainer(i,3);
+                    posDataFound = 1;
                     break;
                 end
             end
-            currentFrame = imageSeries.read();
+            obj.currentFrame = obj.imageSeries.read();
         end
         
         function obj = loadImage(filename)
-            imageSeries = Tiff(strcat(currentDirectory,filename),'r');
-            for i=1:size(metaDataContainer(:,1)
-                metaDataFound = 0;
-                if strcmp(filename,metaDataContainer(i,1))==1
-                    imageMetaData(1) = metaDataContainer(2);
-                    imageMetaData(2) = metaDataContainer(3);
-                    imageMetaData(3) = metaDataContainer(4);
-                    metaDataFound = 1;
+            obj.imageSeries = Tiff(strcat(obj.currentDirectory,filename),'r');
+            for i=1:length(obj.fileMetaDataIndex(:,1))
+                posDataFound = 0;
+                if strcmp(filename,char(obj.fileMetaDataIndex(i,:)))==1
+                    obj.imagePosData(1) = obj.posDataContainer(i,1);
+                    obj.imagePosData(2) = obj.posDataContainer(i,2);
+                    obj.imagePosData(3) = obj.posDataContainer(i,3);
+                    posDataFound = 1;
                     break;
                 end
             end
-            currentFrame = imageSeries.read();
+            obj.currentFrame = obj.imageSeries.read();
+        end
+        
+        function obj = changeDirectory(dir)
+            obj.currentDirectory = dir;
+            newDir = 1;
+            numDir = length(obj.directoryList(:,1));
+            for i=1:numDir
+                if strcmp(dir,char(obj.directoryList(i,:)))==1
+                    newDir = 0;
+                    break;
+                end
+            end
+            if newDir==1
+                obj.directoryList(numDir+1,:) = dir;
+                ifile = length(obj.fileMetaDataIndex(:,1)) + 1;
+                while feof(fid)==0
+                    tmp = fgets(fid);
+                    obj.fileMetaDataIndex(ifile,:) = tmp(1:end-1);
+                    tmp = fscanf(fid,'p=(%f,%f,%f)\n');
+                    obj.posDataContainer(ifile,1) = tmp(1);
+                    obj.posDataContainer(ifile,2) = tmp(2);
+                    obj.posDataContainer(ifile,3) = tmp(3);
+                    ifile = ifile + 1;
+                end
+            end
         end
         
         function obj = loadFrame(index)
-            imageSeries.setDirectory(index);
-            currentFrame = imageSeries.read();
+            obj.imageSeries.setDirectory(index);
+            obj.currentFrame = obj.imageSeries.read();
         end
         
     end
